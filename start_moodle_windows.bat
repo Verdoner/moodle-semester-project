e546ce9da006a1b7abbe3d7839e192de89f86380
AllanVester <vesterallan246@gmail.com>
2024-10-31 10:05:55 +0100

    Updated start_moodle_windows.bat

--- start_moodle_windows.bat
+++ start_moodle_windows.bat
@@ -4,4 +4,4 @@ SET MOODLE_DOCKER_WWWROOT=.\server\moodle
 SET MOODLE_DOCKER_DB=mariadb
 
 echo Starting Moodle Docker Compose services...
-"server\bin\moodle-docker-compose.cmd" up
+"server\bin\moodle-docker-compose.cmd" build && "server\bin\moodle-docker-compose.cmd" up -d
