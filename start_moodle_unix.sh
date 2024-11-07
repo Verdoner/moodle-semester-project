9ba33380ba1eb194bced3646a799f2f6018c88ef
AllanVester <vesterallan246@gmail.com>
2024-11-07 22:54:58 +0100

    Setup for Unix-based instead

--- start_moodle_unix.sh
+++ start_moodle_unix.sh
@@ -8,4 +8,3 @@ export MOODLE_DOCKER_DB=mariadb
 
 echo "Starting Moodle Docker Compose services..."
 server/bin/moodle-docker-compose build && server/bin/moodle-docker-compose up -d
-echo "Moodle services started."
--- start_moodle_windows.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-SET MOODLE_DOCKER_WWWROOT=.\server\moodle
-SET MOODLE_DOCKER_DB=mariadb
-
-echo Starting Moodle Docker Compose services...
-"server\bin\moodle-docker-compose.cmd" build && "server\bin\moodle-docker-compose.cmd" up -d
-echo Moodle services started.
