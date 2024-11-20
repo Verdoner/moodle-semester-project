a91a0995e2301a399d9f1ce2be0f462f6be82c41
Mikkel B. Schmidt <mikkelbuusschmidt@gmail.com>
2024-11-20 14:49:02 +0100

    fået første scenario til at virke

--- server/moodle/blocks/homework/lang/en/block_homework.php
+++ server/moodle/blocks/homework/lang/en/block_homework.php
@@ -27,3 +27,4 @@ $string['homework'] = 'Homework';
 $string['homework block'] = 'homework block';
 $string['homework:addinstance'] = 'Add a new homework block';
 $string['homework:myaddinstance'] = 'Add a new homework block to the My Moodle page';
+$string['pluginname'] = 'Homework';
